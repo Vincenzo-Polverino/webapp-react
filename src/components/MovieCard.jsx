e5655c6e291cde970825d5b7fb96c8b093652f09
Vincenzo-Polverino <94polverino.vincenzo@gmail.com>
2024-12-11 16:55:12 +0100

    add movieCard and element in home page

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+export default function MovieCard({ movie }) {
+    return (
+        <div className="movie card">
+            <div className="card-body">
+                <h4>{movie.title}</h4>
+                <span className="text-muted">By <span>{movie.director}</span></span>
+                <p className="overview mb-3">{movie.overview}</p>
+                <Link to={`/movies/${movie.id}`} className="btn btn-dark">Leggi di più</Link>
+            </div>
+        </div>
+    );
+}
